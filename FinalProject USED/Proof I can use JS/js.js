ce35072d273a7809e501c11254800760ce73f9cc
William Banquier <william.banquier@ucc.on.ca>
2020-04-03 17:02:00 -0400

    Finsihed template, made other parts of the site for the goals

---
+++ FinalProject USED/Proof I can use JS/js.js
@@ -0,0 +1,11 @@
+//var is for global varibles
+//let is for local varibles
+
+var countrys = ["mexico","canada","usa","china","japan","england","france","brazil"];
+
+function onClickRun(){
+	for (var i=0;i<countrys.length;i++){
+		if(i == )
+	}
+}
+onClickRun()
